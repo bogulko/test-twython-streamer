18341f8b9820bfd9ed88dcd41a2f37f4bd37f276
BuildTools <unconfigured@null.spigotmc.org>
2016-08-27 22:04:30 +0200

    Test of twython - streamer

---
+++ streamer.py
@@ -0,0 +1,19 @@
+from twython import TwythonStreamer
+
+from auth import (consumer_key, consumer_secret,access_token, access_token_secret)
+
+class MyStreamer(TwythonStreamer):
+    def on_success(self, data):
+	    if 'text' in data:
+		    username=data['user']['screen_name']
+		    tweet=data['text']
+		    print("@%s: %s" % (username, tweet))
+			
+stream=MyStreamer(
+    consumer_key,
+	consumer_secret,
+	access_token,
+	access_token_secret
+)
+hashtag=str(input("Search:"))
+stream.statuses.filter(track=hashtag)
