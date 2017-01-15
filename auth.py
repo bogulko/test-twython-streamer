2fadf55f00dfad562994aef3df67cfafbbd25739
BuildTools <unconfigured@null.spigotmc.org>
2017-01-15 23:50:11 +0100

    zmena access

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
 consumer_key = 'GbL*********SSS***'
 consumer_secret = '6NmX**********XXX************************'
-access_token = '765********************************'
+access_token = '765****************CCCCC*************'
 access_token_secret = 'wcx**********************************'
