90a225f905f2fd045b41be2c7e821b3862e314ad
BuildTools <unconfigured@null.spigotmc.org>
2017-01-15 23:18:03 +0100

    auth change

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-consumer_key = 'GbL*******************'
+consumer_key = 'GbL*********SSS***'
 consumer_secret = '6NmX**********************************'
 access_token = '765********************************'
 access_token_secret = 'wcx**********************************'
