41884b2fd938d0941c355373439a9fc63142c81f
BuildTools <unconfigured@null.spigotmc.org>
2017-01-15 23:48:13 +0100

    pat

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
 consumer_key = 'GbL*********SSS***'
-consumer_secret = '6NmX**********************************'
+consumer_secret = '6NmX**********XXX************************'
 access_token = '765********************************'
 access_token_secret = 'wcx**********************************'
