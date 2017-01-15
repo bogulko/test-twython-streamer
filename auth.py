b38cd7d816a39851f89ebdd22081eefdf0e1279e
BuildTools <bogulkoorig@gmail.com>
2017-01-16 00:02:23 +0100

    auth

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-consumer_key = 'GbL*********SSS***'
+consumer_key = 'GbL****1234*SSS***'
 consumer_secret = '6NmX**********XXX************************'
 access_token = '765****************CCCCC*************'
 access_token_secret = 'wcx******HV12345************'
