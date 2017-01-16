4c0fa4638ae24684ef6c78b2f64e03e40770f931
BuildTools <bogulkoorig@gmail.com>
2017-01-16 22:03:52 +0100

    Update

--- streamer.py
+++ streamer.py
@@ -3,15 +3,15 @@
 from twython import TwythonStreamer
 
 from auth_fin import (consumer_key, consumer_secret,
-                  access_token, access_token_secret)
+                      access_token, access_token_secret)
 
 
 class MyStreamer(TwythonStreamer):
 
     def on_success(self, data):
         if 'text' in data:
-            username = data['user']['screen_name']
-            tweet = data['text']
+            username = str(data['user']['screen_name'])
+            tweet = str(data['text'])
             print("@%s: %s" % (username, tweet))
 
 stream = MyStreamer(
